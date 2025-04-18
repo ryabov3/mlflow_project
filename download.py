66ae4c006c13ead8a09151d3f6e70acff98639e0
Mikhail Ryabov <163625572+ryabov3@users.noreply.github.com>
2025-04-18 06:32:57 +0000

    Update download.py

--- download.py
+++ download.py
@@ -5,13 +5,10 @@
 
 
 import pandas as pd
-import os
 
 def download_dataset():
     df = pd.read_csv("https://raw.githubusercontent.com/ryabov3/mlflow_project/refs/heads/main/raw/Student_Performance.csv")
-    output_path = os.path.expanduser("~/MLops_work/Student_Performance.csv")
-    df.to_csv(output_path, index=False)
-    os.chmod(output_path, 0o644)
+    df.to_csv("Student_Performance.csv", index=False)
 
 def preprocess_dataset(path):
     df = pd.read_csv(path)
@@ -20,12 +17,10 @@ def preprocess_dataset(path):
     df = df[(df['Hours Studied'] <= 24) & 
             (df['Sleep Hours'] >= 4) & 
             (df['Previous Scores'] >= 40)]
-    output_path = os.path.expanduser("~/MLops_work/clear_Student_Performance.csv")
-    df.to_csv(output_path, index=False)
-    os.chmod(output_path, 0o644)
+    df.to_csv("clear_Student_Performance.csv", index=False)
 
 download_dataset()
-preprocess_dataset(os.path.expanduser("~/MLops_work/Student_Performance.csv"))
+preprocess_dataset("Student_Performance.csv")
 
 
 # In[ ]:
